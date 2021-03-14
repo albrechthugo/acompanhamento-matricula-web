3f66e9d1da46330f0d23bd2a1c25d44d34221b97
Hugo <albrechthugo4@gmail.com>
2021-03-14 16:54:17 -0300

    refactor: using static methods validators

--- src/app/shared/validators/validators.ts
+++ src/app/shared/validators/validators.ts
@@ -1,7 +1,9 @@
 import { FormControl, ValidationErrors } from '@angular/forms';
 import { isValidCPF } from '@brazilian-utils/brazilian-utils';
 
-export function validateCpf(control: FormControl): ValidationErrors | null {
-  return isValidCPF(control.value) ? null : { cpf: false };
+export class MyValidators {
+  static validateCpf(control: FormControl): ValidationErrors | null {
+    return isValidCPF(control.value) ? null : { cpf: false };
+  }
 }
 
