5843ef8e328dde54710d677a4232da5a635853a7
Hugo <albrechthugo4@gmail.com>
2021-04-13 19:04:44 -0300

    chore: create widget component

---
+++ src/app/shared/components/widget/widget.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-widget',
+  templateUrl: './widget.component.html',
+  styleUrls: ['./widget.component.css']
+})
+export class WidgetComponent implements OnInit {
+
+  @Input() icon = '';
+  @Input() label = '';
+  @Input() status = '';
+  @Input() showNote = false;
+  @Input() note = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
